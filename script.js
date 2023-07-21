e676ac8852cba6dc68138ed9f1accd4f1e606eac
na-raghavan <85679113+na-raghavan@users.noreply.github.com>
2023-07-21 07:54:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const button1 = document.getElementById("button1");
-const button2 = document.getElementById("button2");
-const button3 = document.getElementById("button3");
-const iframe = document.getElementById("iframe");
+const checkbox = document.getElementById("checkbox");
+const iframeContainer = document.getElementById("iframeContainer");
+const iframe1 = document.getElementById("iframe1");
+const iframe2 = document.getElementById("iframe2");
 
-// Add click event listeners to the buttons
-button1.addEventListener("click", () => embedContent("https://na-raghavan.github.io/datavisproject1/"));
-button2.addEventListener("click", () => embedContent("https://na-raghavan.github.io/datavisproject1/"));
-button3.addEventListener("click", () => embedContent("https://na-raghavan.github.io/datavisproject1/"));
-
-// Function to embed the selected content using the iframe
-function embedContent(url) {
-  // Set the iframe source to the selected URL
-  iframe.src = url;
-}
+checkbox.addEventListener("change", () => {
+  if (checkbox.checked) {
+    iframe1.style.display = "block";
+    iframe2.style.display = "none";
+  } else {
+    iframe1.style.display = "none";
+    iframe2.style.display = "block";
+  }
+});
